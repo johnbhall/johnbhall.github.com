2a7701ec474638cb54774515b471128338a33dff
John B. Hall <jhall@customink.com>
2011-10-22 02:45:33 -0400

    add iphone 4s re-creation

---
+++ iphone-4s/js/gallery.js
@@ -0,0 +1,83 @@
+$(function() {
+  
+  var phoneStage = $(".phone-stage"),
+      bphone = $(".bphone");
+  
+  var q = new Slide([{
+      el: phoneStage,
+      x: -325,
+      y: -234,
+      r: -30,
+    }, {
+      el: bphone,
+      x: 0,
+      y: 0,
+      r: -30
+    }]),
+  
+  t = new Slide([{
+      el: phoneStage,
+      x: -264,
+      y: -316,
+      r: -0
+    }, {
+      el: bphone,
+      x: -300,
+      y: -300,
+      r: -30
+    }]),
+  
+  z = new Slide([{
+      el: phoneStage,
+      x: -403,
+      y: -186,
+      r: -90
+  }]),
+  
+  u = new Slide([{
+      el: phoneStage,
+      x: -16,
+      y: -374,
+      r: 0
+  }]),
+  
+  j = new Slide([{
+      el: phoneStage,
+      x: -67,
+      y: -398,
+      r: -90
+  }]),
+  
+  f = new Slide([{
+      el: phoneStage,
+      x: -115,
+      y: -326,
+      r: -180
+    }, {
+      el: bphone,
+      x: -300,
+      y: -300,
+      r: -30
+  }]),
+  
+  v = new Slide([{
+      el: phoneStage,
+      x: -323,
+      y: -428,
+      r: -210
+    }, {
+      el: bphone,
+      x: 0,
+      y: 0,
+      r: -30
+  }]);
+  
+  b = new Sequencer([q, t, z, u, j, f, v]);
+        
+  window.addEventListener("load",
+    function() {
+      b.play();
+    },
+    false);
+
+});
---
+++ iphone-4s/js/sequencer.js
@@ -0,0 +1,81 @@
+var Sequencer = function(b) {
+  $.extend(this,
+    {
+      slideTime: 1100,
+      transitionTime: 900,
+      reset: true,
+      sequence: b,
+      current: -1,
+      currentTimeout: undefined,
+      autoPlaying: false,
+      first: true
+    }
+  )
+};
+
+Sequencer.prototype = {
+  
+  next: function() {
+    if(this.current < this.sequence.length-1) {
+      this.current++
+    } else {
+      this.current=0
+    }
+    var a=[];
+    if(this.reset && this.current === this.sequence.length-1) {
+      a.push(function() {
+        this.sequence[0].forward(0);
+        this.current=0
+      });
+      this.sequence[this.current].forward(this.transitionTime, a, this)
+    } else {
+      this.sequence[this.current].forward(this.first ? 0 : this.transitionTime, a, this);
+      this.first=false
+    }
+    if(this.autoPlaying) {
+      var that = this;
+      setTimeout(function() {
+        that.next()
+      },5500)
+    }
+  },
+  
+  play: function() {
+    this.autoPlaying = true;
+    this.next()
+  }
+  
+};
+
+function Slide(a) {
+  this.seq=a;
+}
+
+Slide.prototype = {
+  
+  forward: function(transitionTime, additionalInstructions, sequencer) {
+    if(additionalInstructions && additionalInstructions.length > 0 && sequencer && transitionTime > 0) {
+      var e = this.seq[0].el;
+      var a = function() {
+        additionalInstructions[0].call(sequencer);
+        e[0].removeEventListener("webkitTransitionEnd", a, false)
+      };
+      e[0].addEventListener("webkitTransitionEnd", a, false)
+    }
+    var that = this
+    $.each(this.seq, function(i, v) {
+      that.transform(v, "forward", transitionTime);
+    })
+  },
+
+  transform: function(b, delayType, transitionTime) {
+    b.el.css("-webkit-transition", transitionTime + "ms cubic-bezier(.51, .01, .37, .98)");
+    b.el.css("-webkit-transform", this.transformString(b.x, b.y, b.r))
+  },
+  
+  transformString: function(x, y, r) {
+    r = r ? r : 0;
+    return "translate(" + x + "px, " + y + "px) rotate(" + r + "deg)"
+  }
+  
+};
